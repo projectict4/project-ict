60893f5cb4a7c796da90932ce0f838e4b5a8a33a
Bobart <bartbogaerts@hotmail.com>
2014-05-03 20:16:13 +0200

    SQL query's

--- phonegap built/get-translation-info.php
+++ phonegap built/get-translation-info.php
@@ -16,6 +16,31 @@
 		$info = mysql_result($data,0,'phrase');
 		//$info = mysql_fetch_array( $data );
 		//$infoString = (string)$info;
+		
+		
+		// SQL query for all traveling sentences in english SELECT phrase FROM `phrase english` WHERE `cat id`=1
+		// SQL query for all directions sentences in english SELECT phrase FROM `phrase english` WHERE `cat id` = 2
+		// SQL query for all ordering and shopping sentences in english SELECT phrase FROM `phrase english` WHERE `cat id` = 3
+		// SQL query for all thanking and apologicing sentences in english SELECT phrase FROM `phrase english` WHERE `cat id` = 4
+		// SQL query for all dates and times sentences in english SELECT phrase FROM `phrase english` WHERE `cat id` = 5
+		// SQL query for all numerals sentences in english SELECT phrase FROM `phrase english` WHERE `cat id` = 6
+		
+		
+		// SQL query for all traveling sentences in english SELECT phrase FROM `phrase dutch` WHERE `cat id`=1
+		// SQL query for all directions sentences in english SELECT phrase FROM `phrase dutch` WHERE `cat id` = 2
+		// SQL query for all ordering and shopping sentences in english SELECT phrase FROM `phrase dutch` WHERE `cat id` = 3
+		// SQL query for all thanking and apologicing sentences in english SELECT phrase FROM `phrase dutch` WHERE `cat id` = 4
+		// SQL query for all dates and times sentences in english SELECT phrase FROM `phrase dutch` WHERE `cat id` = 5
+		// SQL query for all numerals sentences in english SELECT phrase FROM `phrase dutch` WHERE `cat id` = 6
+		
+		
+		// SQL query for all traveling sentences in english SELECT phrase FROM `phrase finnish` WHERE `cat id`=1
+		// SQL query for all directions sentences in english SELECT phrase FROM `phrase finnish` WHERE `cat id` = 2
+		// SQL query for all ordering and shopping sentences in english SELECT phrase FROM `phrase finnish` WHERE `cat id` = 3
+		// SQL query for all thanking and apologicing sentences in english SELECT phrase FROM `phrase finnish` WHERE `cat id` = 4
+		// SQL query for all dates and times sentences in english SELECT phrase FROM `phrase finnish` WHERE `cat id` = 5
+		// SQL query for all numerals sentences in english SELECT phrase FROM `phrase finnish` WHERE `cat id` = 6
+		
 
 		//echo $_GET['callback'] . '(' . "{'text' : $infoString}" . ')';
 		//echo $_GET['callback'] . '(' . "{'text' : 'test'}" . ')';
