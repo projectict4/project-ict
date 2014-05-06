a694447edc62163d39113c48501d6d0c41ca9d75
sander <sander.y@hotmail.com>
2014-05-06 20:33:46 +0200

    zinnen worden naar index gestuurd

--- phonegap built/get-translation-info.php
+++ phonegap built/get-translation-info.php
@@ -15,9 +15,25 @@
 		$language1 = $_GET['language1'];
 		$language2 = $_GET['language2'];
 
+
+
 		// Collects phrases in table phrase english 
 		$data = mysql_query("SELECT phrase FROM `phrase english` WHERE id='1'");
 		$info = mysql_result($data,0,'phrase');
+
+		$data2 = mysql_query("SELECT phrase FROM `$language1` WHERE id = ANY (SELECT `id eng` FROM `phrase connections` WHERE cat='$cat')");
+		$total_rows = mysql_num_rows($data2);
+		//$info2 = mysql_result($data2,0,'phrase');
+
+		$array_result = array();
+		for ($i = 1; $i <= $total_rows; $i++) {
+			$j = $i - 1;
+			$array_result["key$i"] = mysql_result($data2,$j,'phrase');
+		}
+		//echo $array_result[1];
+
+		//$info3 = mysql_fetch_array($data2);
+
 		//$info = mysql_fetch_array( $data );
 		//$infoString = (string)$info;
 		
@@ -46,14 +62,18 @@
 		// SQL query for all numerals sentences in english SELECT phrase FROM `phrase finnish` WHERE `cat id` = 6
 		
 
-		//echo $_GET['callback'] . '(' . "{'text' : $infoString}" . ')';
-		//echo $_GET['callback'] . '(' . "{'text' : 'test'}" . ')';
-
-		$jsonstring = json_encode(''. $info . '');
-		echo $_GET['callback'] . '(' . "{'text' : $jsonstring}" . ')';
+		//$jsonstring = json_encode(''. $info2 . '');
+		//echo $_GET['callback'] . '(' . "{'text' : $jsonstring}" . ')';
 
 		//$jsonstring2 = json_encode(''. $language1 . '');
 		//echo $_GET['callback'] . '(' . "{'text' : $jsonstring2}" . ')';
+
+
+		$json =  json_encode($array_result);
+		echo $_GET['callback'] . '(' . $json . ')';
+
+		//$jsonstring = json_encode(''. $array_result[1] . '');
+		//echo $_GET['callback'] . '(' . "{'text' : $jsonstring}" . ')';
 	}
 	else
 	{
